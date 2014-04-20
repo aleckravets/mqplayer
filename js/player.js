5dcb5671a6bad9d9a057387bf1eb36d75970ac16
alex <alec.kravets@gmail.com>
2014-04-20 16:22:45 +0400

    drag and drop

--- js/player.js
+++ js/player.js
@@ -4,17 +4,17 @@ angular.module('Player', [])
     .directive('player', ['Player', function(Player) {
         return {
             restrict: 'E',
-            scope: { },
+            scope: {},
             controller: function($scope, $element) {
                 $scope.player = Player;
 
                 $scope.select = function($event, item) {
                     if ($scope.selectedItem)
-                        $scope.selectedItem.selected = false;
+                        $scope.selectedItem.selected2 = false;
 
                     $scope.selectedItem = item;
 
-                    item.selected = true;
+                    item.selected2 = true;
                 };
 
                 Player.audio.addEventListener('ended', function() {
@@ -24,14 +24,38 @@ angular.module('Player', [])
             templateUrl: 'player.html'
         };
     }])
-    .factory('Player', function() {
+    .directive('droppable', ['Player', 'TreeState', function(Player, TreeState) {
+        return {
+            link: function(scope, element, attrs) {
+                element.on('dragover', function(e) {
+                    e.preventDefault(); // allow drop
+                });
+
+                element.on('dragenter', function(e) {
+                    scope.$apply(function(){
+                        scope.item.dragover = true;
+                    });
+                });
+
+                element.on('dragleave', function(e) {
+                    scope.item.dragover = false;
+                });
+
+                element.on('drop', function(e) {
+                    scope.item.dragover = false;
+                    Player.enqueue(TreeState.dragging, scope.item);
+                });
+            }
+        }
+    }])
+    .factory('Player', ['$q', function($q) {
         var audio = new Audio();
         var playlist = [];
 
         var player = {
             audio: audio,
             playlist: playlist,
-            play: function(item) {
+            playItem: function(item) {
                 audio.src = item.getUrl();
                 audio.play();
                 audio.currentItem = item;
@@ -42,52 +66,70 @@ angular.module('Player', [])
             isSupportedType: function(item) {
                 return /\.mp3/.test(item.href);
             },
+            play: function(item) {
+                this.stop();
+                this.clearPlaylist();
+
+                var th = this;
+
+                this.getPlayableItems(item).then(function(items) {
+                    items.forEach(function(item) {
+                        playlist.push(item);
+                    });
+
+                    if (playlist.length > 0)
+                        th.playItem(playlist[0]);
+                });
+            },
+            getPlayableItems: function(item) {
+                var th = this;
+                return $q.when(item.isDir() ? item.getChildren() : [item]).then(function(items) {
+                    var playable = [];
+                    items.forEach(function(item) {
+                        if (!item.isDir() && th.isSupportedType(item))
+                            playable.push(item);
+                    });
+
+                    return playable;
+                });
+            },
+            enqueue: function(item, insertBeforeItem) {
+                this.getPlayableItems(item).then(function(items) {
+                    if (insertBeforeItem) {
+                        var index = playlist.indexOf(insertBeforeItem);
+                        items.forEach(function(item){
+                            playlist.splice(index++, 0, item);
+                        });
+                    }
+                    else {
+                        items.forEach(function(item){
+                            playlist.push(item);
+                        });
+                    }
+                });
+            },
             prev: function() {
                 var i = playlist.indexOf(audio.currentItem);
                 if (i - 1 >= 0)
-                    this.play(playlist[i - 1]);
+                    this.playItem(playlist[i - 1]);
             },
             next: function() {
                 var i = playlist.indexOf(audio.currentItem);
                 if (i + 2 <= playlist.length)
-                    this.play(playlist[i + 1]);
+                    this.playItem(playlist[i + 1]);
             },
             stop: function() {
                 audio.pause();
                 audio.src = '';
                 audio.currentItem = null;
             },
-            setAndPlay: function(item) {
-                this.stop();
-                this.clearPlaylist();
-
-                if (item.isDir()) {
-                    var th = this;
-                    item.getChildren().then(function(items) {
-                        for (var i = 0; i < items.length; i++) {
-                            if (th.isSupportedType(items[i])) {
-                                playlist.push(items[i]);
-                            }
-                        }
-
-                        if (playlist.length > 0)
-                            th.play(playlist[0]);
-                    });
-                }
-                else {
-                    if (this.isSupportedType(item))
-                        playlist.push(item);
-
-                    this.play(item);
-                }
-            },
             playPause: function() {
                 if (audio.paused) {
                     if (audio.currentItem) {
-                        audio.play()
+                        audio.playItem()
                     }
                     else if (playlist) {
-                        this.play(playlist[0]);
+                        this.playItem(playlist[0]);
                     }
                 }
                 else {
@@ -101,4 +143,4 @@ angular.module('Player', [])
         });
 
         return player;
-    });
+    }]);
--- js/tree.js
+++ js/tree.js
@@ -1,26 +1,58 @@
 'use strict';
 
 angular.module('Tree', [])
-    .controller('TreeCtrl', ['$scope', 'Player', 'Item', function($scope, Player, Item) {
-        var root = new Item();
+    .directive('tree', ['Player', 'Item', function(Player, Item) {
+        return {
+            restrict: 'E',
+            scope: { },
+            controller: function($scope, $element) {
+                var root = new Item();
 
-        root.getChildren().then(function(items) {
-            $scope.items = items;
-        });
+                root.getChildren().then(function(items) {
+                    $scope.items = items;
+                });
 
-        $scope.toggleDir = function(item) {
-            item.getChildren();
-            item.collapsed = !item.collapsed;
+                $scope.toggleDir = function(item) {
+                    item.getChildren();
+                    item.collapsed = !item.collapsed;
+                };
+
+                $scope.select = function($event, item) {
+                    if ($scope.selectedItem)
+                        $scope.selectedItem.selected = false;
+
+                    $scope.selectedItem = item;
+
+                    item.selected = !item.selected;
+                };
+
+                $scope.player = Player;
+            },
+            link: function(scope, element, attrs) {
+
+            },
+            templateUrl: 'tree.html'
         };
+    }])
+    .directive('draggable', ['TreeState', function(TreeState) {
+        return {
+            link: function(scope, element, attrs) {
+                var item = scope.item;
 
-        $scope.select = function($event, item) {
-            if ($scope.selectedItem)
-                $scope.selectedItem.selected = false;
+                element.attr('draggable', true);
 
-            $scope.selectedItem = item;
+                element.on('dragstart', function(e) {
+                    e.dataTransfer.setData('text/html', ''); // needed for FF.
+                    TreeState.dragging = item;
+                });
 
-            item.selected = !item.selected;
+                element.on('dragend', function() {
+                    delete TreeState.dragging;
+                });
+            }
         };
-
-        $scope.player = Player;
-    }]);
+    }])
+    .factory('TreeState', function(){
+        return { };
+    })
+;
