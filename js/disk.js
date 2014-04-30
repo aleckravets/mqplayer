53065c58be566988a6a85674880fee6b82318f9e
aleckravets <alec.kravets@gmail.com>
2014-04-30 11:55:27 +0400

    cleanup

--- js/disk.js
+++ js/disk.js
@@ -1,33 +1,5 @@
 'use strict';
 
-angular.module('Disk', ['Tree', 'Player', 'Services'])
-    .factory('Item', ['$q', 'DiskAPI', function($q, DiskAPI) {
-        function Item(itemData) {
-            angular.extend(this, itemData || { href: '/' });
-        }
+angular.module('Disk', ['Tree', 'Player', 'Services']);
 
-        Item.prototype = {
-            collapsed: true,
-            isDir: function() {
-                return this.resourceType == 'dir';
-            },
-            getChildren: function() {
-                var item = this;
-                if (!this.children)
-                    this.loading = true;
-                return $q.when(this.children || DiskAPI.loadItemsData(this.href).then(function(response) {
-                    item.loading = false;
-                    item.children = response.data.map(function(itemData){
-                        return new Item(itemData);
-                    });
-                    return item.children;
-                }));
-            },
-            getUrl: function() {
-                return DiskAPI.getFileUrl(this.href)
-            }
-        };
-
-        return Item;
-    }]);
 
---
+++ js/item.js
@@ -0,0 +1,32 @@
+'use strict';
+
+angular.module('Disk')
+    .factory('Item', ['$q', 'DiskAPI', function($q, DiskAPI) {
+        function Item(itemData) {
+            angular.extend(this, itemData || { href: '/' });
+        }
+
+        Item.prototype = {
+            collapsed: true,
+            isDir: function() {
+                return this.resourceType == 'dir';
+            },
+            getChildren: function() {
+                var item = this;
+                if (!this.children)
+                    this.loading = true;
+                return $q.when(this.children || DiskAPI.loadItemsData(this.href).then(function(response) {
+                    item.loading = false;
+                    item.children = response.data.map(function(itemData){
+                        return new Item(itemData);
+                    });
+                    return item.children;
+                }));
+            },
+            getUrl: function() {
+                return DiskAPI.getFileUrl(this.href)
+            }
+        };
+
+        return Item;
+    }]);
