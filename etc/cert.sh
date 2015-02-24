9904a98a0c9524baf87eff5a3833514118a394ef
alec kravets <alec.kravets@gmail.com>
2015-02-24 10:53:30 +0300

    readme

--- etc/cert.sh
+++
@@ -1,7 +0,0 @@
-# downalod the certificate from https://content.googleapis.com to c:\gapi.cer
-# by going to https://content.googleapis.com/drive/v2/about for example
-# import the certificate to keystore by the following commands
-
-cd %JAVA_HOME%/lib/security
-
-keytool -import -alias ca -file c:\gapi.cer -keystore cacerts -storepass changeit
